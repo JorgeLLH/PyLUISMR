16a7429a7365f366bc98ad9645d8495bf8f13613
Resairchia <88433441+JorgeLLH@users.noreply.github.com>
2022-09-18 23:44:44 +0200

    Update pyLUISMR.py
    Update to add the latest features, among them FFT analysis.

--- PyLUISMR/pyLUISMR.py
+++ PyLUISMR/pyLUISMR.py
@@ -14,7 +14,37 @@ def clickEvent(event):
     print(results_list_Box.curselection()[0])
     clicked_position=results_list_Box.curselection()[0]
     plot_results_function(sol, clicked_position)
+    ##Plot FFT if requested
+    if (checkBox_FFT_var.get() == 1):
+        plotFFT_results_function(sol, clicked_position)
 #click results listboxes end--------------
+
+#plotting FFT
+def plotFFT_results_function(sol, clicked_position):
+    t_start=start_Box.get(1.0, tk.END+"-1c")
+    t_stop=stop_Box.get(1.0, tk.END+"-1c")
+    t_step=step_Box.get(1.0, tk.END+"-1c")
+    #Sampling variables
+    intervals=eval(t_step)
+    frequency_intervals=1/intervals
+    signal_start_time=eval(t_start)
+    signal_end_time=eval(t_stop)
+    transform_Fourier=np.fft.fft(sol.y[(clicked_position - 1)])/len(sol.y[(clicked_position - 1)])
+    transform_Fourier=transform_Fourier[range(int(len(sol.y[(clicked_position - 1)])/2))]
+    points_Count=len(sol.y[(clicked_position - 1)])
+    point_Values=np.arange(int(points_Count/2))
+    time_Period=points_Count/frequency_intervals
+    resultant_Frequencies=point_Values/time_Period
+    #eval(t_start), eval(t_stop)+eval(t_step)
+    #
+    plt.style.use('seaborn-poster')
+    if (clicked_position - 1) > -1:
+        plt.figure(figsize = (12, 4))
+        plt.plot(resultant_Frequencies, 2*abs(transform_Fourier))
+        plt.show()
+    else:
+        messagebox.showinfo('information', 'Click on a variable to plot its values')
+#plotting FFT end
     
 #plotting function------------------------
 def plot_results_function(sol, clicked_position):
@@ -41,7 +71,11 @@ def plot_results_function(sol, clicked_position):
 #plotting function end----------------------
         
 def simulate_function():
-
+    ##Simulation setup
+    t_start=start_Box.get(1.0, tk.END+"-1c")
+    t_stop=stop_Box.get(1.0, tk.END+"-1c")
+    t_step=step_Box.get(1.0, tk.END+"-1c")
+    ##
     #F = lambda t, s: np.cos(t)
     F=text_Box.get(1.0, tk.END+"-1c")
 
@@ -54,31 +88,49 @@ def simulate_function():
     if var_names != None:
         aux_var_names=re.sub('Vars<<', '', var_names.group())
         aux_var_names=re.sub('>>Vars', '', aux_var_names)
-        results_list_Box.insert(1,aux_var_names)
+        ##split using commas
+        aux_var_names=aux_var_names.split(',')
+        for i_var_names in range(len(aux_var_names)):
+            results_list_Box.insert((i_var_names+1),aux_var_names[i_var_names])
     F=re.sub('\nVars<<.+>>Vars', '', F)
     #Search&remove variable names end-------
+
+    ##Search&remove variable initializations--------------
+    x_init = re.search(r"Init<<.+>>Init", F)
+    if x_init != None:
+        auxx_init=re.sub('Init<<', '', x_init.group())
+        auxx_init=re.sub('>>Init', '', auxx_init)
+        x_init=auxx_init
+        if len(x_init.split(',')) == 1:
+            x_init='['+x_init+']'
+            
+
+    F=re.sub('\nInit<<.+>>Init', '', F)
+    ##Search&remove variable  initializations end--------------
+
+    ##add lambda t to the first line to simplify
+    F='lambda t, '+F
     
     #clean input
     F=re.sub('\n', ', ', F)
-    #Simulation setup
-    t_start=start_Box.get(1.0, tk.END+"-1c")
-    t_stop=stop_Box.get(1.0, tk.END+"-1c")
-    t_step=step_Box.get(1.0, tk.END+"-1c")
-    #
+
     if len(F) == 0 :
         messagebox.showerror("Error", "No system of equations.")
     else:
         #arange: (start, stop, step)
-        t_eval = np.arange(eval(t_start), eval(t_stop), eval(t_step))
+        ##Added the last time step to the simulation time
+        t_eval = np.arange(eval(t_start), eval(t_stop)+eval(t_step), eval(t_step))
         #change time limit------------------
         #store global results
         global sol
-        sol = solve_ivp(eval(F), [0, eval(t_stop)], [0], t_eval=t_eval)
+        ##Include the initial values array x_init
+        sol = solve_ivp(eval(F), [0, eval(t_stop)], eval(x_init), t_eval=t_eval)
 
-        #populate list
-        results_list_Box2.insert(1,sol.y[0][0])
-        results_list_Box3.insert(1,sol.y[0][-1])
-        #populate list end------------------
+        ##populate list
+        for i_var_results in range(len(sol.y)):
+            results_list_Box2.insert((i_var_results+1),sol.y[i_var_results][0])
+            results_list_Box3.insert((i_var_results+1),sol.y[i_var_results][-1])
+        ##populate list end
 
 #tk root configuration----------------------
 root = tk.Tk()
@@ -130,6 +182,11 @@ results_list_Box = tk.Listbox(frm, height=10, width=30, listvariable=results_lis
 results_list_Box.grid(column=4, row=2)
 results_list_Box.insert(0, 'Variable name')
 results_list_Box.bind('<<ListboxSelect>>', clickEvent)
+##checkbox to show FFT
+checkBox_FFT_var = tk.IntVar()
+checkBox_FFT = tk.Checkbutton(frm, text='Show FFT analysis',variable=checkBox_FFT_var, onvalue=1, offvalue=0)
+checkBox_FFT.grid(column=5, row=1)
+##
 #
 
 results_list_var2 = tk.StringVar()
